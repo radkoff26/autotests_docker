27b543b8811c9f9cc922afb6a7e5786a36714a99
radkoff26 <radko.slava@mail.ru>
2023-04-26 18:35:37 +0300

    initial commit

---
+++ src/main/java/com/example/autotests_docker/AutotestsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.autotests_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestsDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutotestsDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autotests_docker/AutotestsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autotests_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotestsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
